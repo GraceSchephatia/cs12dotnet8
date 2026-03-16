28a4b115fb7137741a630be7cc21862774421433
Bly, Grace Schephatia (CSC SCC) <GraceSchephatia.Bly@CSC-SCC.GC.CA>
2026-03-16 15:38:06 -0400

    First commit

---
+++ Chapter01/HelloCS/Program.cs
@@ -0,0 +1,12 @@
+﻿// Revealing the namespace for the Program class
+
+/*
+ * ?? is the null-coalescing operator
+ * If the namespace of Program is null, then return None!; otherwise, return the name.
+ */
+
+string name = typeof(Program).Namespace ?? "None";
+Console.WriteLine($"Namespace: {name}");
+
+throw new Exception();
+int z;
---
+++ Chapter02/Vocabulary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //#error version
+
+            //Console.WriteLine("Hello World!");
+            WriteLine($"Computer named {Env.MachineName} says \"No.\"");
+
+            #region Three variables that store the number 2 million.
+            int decimalNotation = 2_000_000;
+            int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
+            int hexadecimalNotation = 0x_001E_8480;
+            #endregion
+
+            // Outputs the current line terminator.
+            // By default, this is a carriage-return and line feed.
+            Console.WriteLine();
+            // Outputs the greeting and the current line terminator.
+            Console.WriteLine("Hello Ahmed");
+            // Outputs a formatted number and date and the current line terminator.
+            Console.WriteLine(
+            "Temperature on {0:D} is {1}°C.", DateTime.Today, 23.4);
+        }
+    }
+}
