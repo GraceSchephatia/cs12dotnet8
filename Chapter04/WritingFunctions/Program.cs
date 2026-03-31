7a55e4aa1113a60c06e37538918d50239ad1f374
Grace Schephatia Bly <BlyGS@csc-scc.gc.ca>
2026-03-31 15:04:16 -0400

    Add Chapter04; Add Top Level Functions and Writting Functions projects

---
+++ Chapter04/WritingFunctions/Program.cs
@@ -0,0 +1,7 @@
+﻿// TimesTable(7,20);
+
+ConfigureConsole(culture:"fr-FR");
+//ConfigureConsole(useComputerCulture: true);
+
+decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "CA");
+WriteLine($"You must pay {taxToPay:C} in tax.");
