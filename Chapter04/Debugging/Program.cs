a840b16c541a39601f173ef0b6d8aabbca9f8d90
Grace Schephatia Bly <BlyGS@csc-scc.gc.ca>
2026-04-27 08:48:57 -0400

    Add Debugging project

---
+++ Chapter04/Debugging/Program.cs
@@ -0,0 +1,14 @@
+﻿double a = 4.5;
+double b = 2.5;
+double answer = Add(a, b);
+
+
+double Add(double a, double b)
+{
+    return a * b;  // Deliberate bug
+}
+
+WriteLine($"{a} + {b} = {answer}");
+
+WriteLine("Press Enter to end the app.");
+ReadLine(); // Wait for user to press Enter 
