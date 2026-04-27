1fd2a81f044a5fd4267b6454b02d9ed3c398eb58
Grace Schephatia Bly <BlyGS@csc-scc.gc.ca>
2026-04-27 08:50:54 -0400

    Add WritingFunctions project

--- Chapter04/WritingFunctions/Program.Functions.cs
+++ Chapter04/WritingFunctions/Program.Functions.cs
@@ -47,4 +47,115 @@ partial class Program
     }
     #endregion
 
+    #region Converting numbers from cardinal to ordinal
+    /// <summary>
+    /// Pass a 32-bit unsigned integer and it will be converted into its ordinal equivalent.
+    /// </summary>
+    /// <param name="number">Number as a cardinal value e.g. 1, 2, 3, and so on.</param>
+    /// <returns>Number as an ordinal value e.g. 1st, 2nd, 3rd, and so on.</returns>
+
+    static string CardinalToOrdinal(uint number)
+    {
+        uint lastTwoDigits = number % 100;
+
+        switch (lastTwoDigits)
+        {
+            case 11: // Special cases for 11th to 13th.
+            case 12:
+            case 13:
+                return $"{number:N0}th";
+            default:
+                uint lastDigit = number % 10;
+                string suffix = lastDigit switch
+                {
+                    1 => "st",
+                    2 => "nd",
+                    3 => "rd",
+                    _ => "th"
+                };
+                return $"{number:N0}{suffix}";
+        }
+    }
+
+    static void RunCardinalToOrdinal()
+    {
+        for (uint number = 1; number <= 1500; number++)
+        {
+            Write($"{CardinalToOrdinal(number)} ");
+        }
+        WriteLine();
+    }
+
+    #endregion
+
+    #region Calculating factorials with recursion
+
+    static int Factorial(int number)
+    {
+        if (number < 0)
+        {
+            throw new ArgumentOutOfRangeException(message: $"The factorial function is defined for non-negative integers only.Input: {number}", paramName: nameof(number));
+        }
+        else if (number == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return number * Factorial(number - 1);
+        }
+    }
+
+    static void RunFactorial()
+    {
+        for (int i = -2; i <= 15; i++)
+        {
+            try
+            {
+                WriteLine($"{i}! = {Factorial(i):N0}");
+            }
+            catch (OverflowException)
+            {
+
+                WriteLine($"{i}! is too big for a 32-bit integer.");
+            }
+            catch (Exception ex)
+            {
+                WriteLine($"{i}! throws {ex.GetType()}: {ex.Message}");
+            }
+        }
+    }
+    #endregion
+
+    #region Using lambdas in function implementations
+    static int FibImperative(uint term)
+    {
+        if (term == 0)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+        else if (term == 1)
+        {
+            return 0;
+        }
+        else if (term == 2)
+        {
+            return 1;
+        }
+        else
+        {
+            return FibImperative(term - 1) + FibImperative(term - 2);
+        }
+    }
+
+    static void RunFibImperative()
+    {
+        for (uint i = 1; i <= 30; i++)
+        {
+            WriteLine("The {0} term of the Fibonacci sequence is {1:N0}", 
+                arg0: CardinalToOrdinal(i),
+                arg1: FibImperative(term: 1));
+        }
+    }
+    #endregion
 }
--- Chapter04/WritingFunctions/Program.cs
+++ Chapter04/WritingFunctions/Program.cs
@@ -1,7 +1,11 @@
 ﻿// TimesTable(7,20);
 
-ConfigureConsole(culture:"fr-FR");
+ConfigureConsole(useComputerCulture:true);
 //ConfigureConsole(useComputerCulture: true);
 
-decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "CA");
-WriteLine($"You must pay {taxToPay:C} in tax.");
+//decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "CA");
+//WriteLine($"You must pay {taxToPay:C} in tax.");
+
+//RunCardinalToOrdinal();
+//RunFactorial();
+RunFibImperative();
